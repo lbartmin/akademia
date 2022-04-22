f9ff4c76145885cc41bd4f416b2907d708119a51
lbartmin <bartminski.leszek@gmail.com>
2022-04-22 15:15:58 +0200

    commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "vm_instance" {
+  project      = "semiotic-quasar-302312"
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}

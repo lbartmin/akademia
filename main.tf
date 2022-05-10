cabbc951d2625ebae54ce76d4d813a955ce3b5d0
lbartmin <bartminski.leszek@gmail.com>
2022-05-10 16:02:38 +0200

    Message

--- main.tf
+++ main.tf
@@ -1,18 +1,87 @@
-resource "google_compute_instance" "vm_instance" {
-  project      = "semiotic-quasar-302312"
-  name         = "terraform-instance"
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] 
+  target_tags   = ["allowssh"]
+}
+
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] 
+  target_tags   = ["allowhttps"]
+}
+
+
+resource "google_compute_address" "static" {
+  name = "vm-public-address"
+  project = var.project
+  region = var.region
+  depends_on = [ google_compute_firewall.allow_ssh ]
+}
+
+
+resource "google_compute_instance" "dev" {
+  name         = "devserver"
   machine_type = "f1-micro"
-  zone         = "us-central1-a"
+  zone         = "${var.region}-a"
+  tags         = ["allowssh","allowhttps"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
 
   network_interface {
     network = "default"
+
     access_config {
+      nat_ip = google_compute_address.static.address
     }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = google_compute_address.static.address
+      type        = "ssh"
+      user        = var.user
+      timeout     = "500s"
+      private_key = file(var.privatekeypath)
+    }
+
+    inline = [
+        "#! /bin/bash sudo apt-get update -y && sudo apt-get upgrade -y", 
+        "sudo apt-get -y install nginx",
+        "sudo nginx -v"
+    ]
+  }
+
+  depends_on = [ google_compute_firewall.allow_ssh, google_compute_firewall.allow_https ]
+
+  service_account {
+    email  = var.email
+    scopes = ["compute-ro"]
+  }
+
+  metadata = {
+    ssh-keys = "${var.user}:${file(var.publickeypath)}"
   }
 }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+project = "semiotic-quasar-302312"
+region = "us-central1"
+user = "root"
+email = "tf-serviceaccount@semiotic-quasar-302312.iam.gserviceaccount.com"
